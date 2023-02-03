7fef579a582ee48a6605b73ee14d8fa7c2994a1e
Alex Saveau <saveau.alexandre@gmail.com>
2023-02-02 20:14:03 -0800

    Move instructions out of the decoder module since they'll be used elsewhere

--- hard/lib/decoder.ml
+++ hard/lib/decoder.ml
@@ -1,51 +1,5 @@
 open Hardcaml
 
-module Instruction = struct
-  module RV32I = struct
-    type t =
-      | Lui (* Load upper immediate *)
-      | Auipc (* Add upper immediate to program counter *)
-      | Jal (* Jump and link *)
-      | Jalr (* Jump and link register *)
-      | Beq (* Branch if equal *)
-      | Bne (* Branch if not equal *)
-      | Blt (* Branch if less than *)
-      | Bge (* Branch if greater than or equal *)
-      | Bltu (* Branch if less than unsigned *)
-      | Bgeu (* Branch if greater than or equal unsigned *)
-      | Lb (* Load byte *)
-      | Lh (* Load halfword *)
-      | Lw (* Load word *)
-      | Lbu (* Load byte unsigned *)
-      | Lhu (* Load halfword unsigned *)
-      | Sb (* Store byte *)
-      | Sh (* Store halfword *)
-      | Sw (* Store word *)
-      | Addi (* Add immediate *)
-      | Slti (* Set less than immediate *)
-      | Sltiu (* Set less than immediate unsigned *)
-      | Xori (* Exclusive or immediate *)
-      | Ori (* Or immediate *)
-      | Andi (* And immediate *)
-      | Slli (* Shift left logical immediate *)
-      | Srli (* Shift right logical immediate *)
-      | Srai (* Shift right arithmetic immediate *)
-      | Add (* Add *)
-      | Sub (* Subtract *)
-      | Sll (* Shift left logical *)
-      | Slt (* Set less than *)
-      | Sltu (* Set less than unsigned *)
-      | Xor (* Exclusive or *)
-      | Srl (* Shift right logical *)
-      | Sra (* Shift right arithmetic *)
-      | Or (* Or *)
-      | And (* And *)
-    [@@deriving sexp_of, compare, enumerate]
-  end
-
-  include Hardcaml.Interface.Make_enums (RV32I)
-end
-
 module I = struct
   type 'a t = { instruction : 'a [@bits 32] } [@@deriving sexp_of, hardcaml]
 end
