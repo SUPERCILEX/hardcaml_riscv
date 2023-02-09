97bccd9ca5036520198dd054d936de3fe6e1f385
Alex Saveau <saveau.alexandre@gmail.com>
2023-02-09 13:07:15 -0800

    Do HW4
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- soft/src/main.rs
+++ soft/src/main.rs
@@ -1,24 +1,51 @@
 use core::arch::global_asm;
+use std::{ffi::CString, ptr};
 
 #[cfg(not(target_arch = "x86_64"))]
-global_asm!(include_str!("fibonacci.s"));
+global_asm!(include_str!("atoi.s"));
 #[cfg(target_arch = "x86_64")]
-global_asm!("fib:");
+global_asm!("atoi:");
 
 extern "C" {
-    fn fib(n: u32) -> u32;
+    fn atoi(s: *const u8) -> i32;
 }
 
 fn main() {
-    for n in 0..25 {
-        assert_eq!(fib_rust(n), unsafe { fib(n) });
+    for s in [
+        "123",
+        "-123",
+        "+123",
+        "0",
+        "-0",
+        "+0",
+        "1234567890",
+        "",
+        "+-123",
+    ] {
+        let cstr = CString::new(s).unwrap();
+        assert_eq!(atoi_rust(s.as_bytes()), unsafe {
+            atoi(cstr.as_ptr() as *const _)
+        });
     }
+    assert_eq!(0, unsafe { atoi(ptr::null()) });
 }
 
-fn fib_rust(n: u32) -> u32 {
-    if n <= 1 {
-        n
-    } else {
-        fib_rust(n - 1) + fib_rust(n - 2)
+fn atoi_rust(mut s: &[u8]) -> i32 {
+    let mut value = 0i32;
+    let mut mul = 1;
+    if s.is_empty() {
+        return 0;
     }
+
+    if s[0] == b'+' {
+        s = &s[1..];
+    }
+    if s[0] == b'-' {
+        s = &s[1..];
+        mul = -1;
+    }
+    for s in s {
+        value = value * 10 + i32::from(*s - b'0');
+    }
+    value * mul
 }
