7ebb1f3e3c92e5fded2ec9a50e7336cffc991b17
Alex Saveau <saveau.alexandre@gmail.com>
2023-02-09 20:13:02 -0800

    Compile arty instead of hardware-less verilog

--- hard/cpu/bin/cli.ml
+++ hard/cpu/bin/cli.ml
@@ -1,28 +1,4 @@
 open! Core
-open Hardcaml
-
-let compile =
-  Command.basic
-    ~summary:"Compile into verilog"
-    Command.Let_syntax.(
-      let%map_open filename =
-        flag ~doc:"Output file" "-output" (optional Filename_unix.arg_type)
-      in
-      fun () ->
-        let scope = Scope.create () in
-        Rtl.output
-          ~output_mode:
-            (match filename with
-             | None -> Rtl.Output_mode.To_channel Stdio.Out_channel.stdout
-             | Some file -> Rtl.Output_mode.To_file file)
-          ~database:(Scope.circuit_database scope)
-          Verilog
-          (Circuit.create_with_interface
-             (module Cpu.I)
-             (module Cpu.O)
-             ~name:"cpu_top"
-             (Cpu.circuit scope)))
-;;
 
 let waves =
   Command.basic
@@ -31,8 +7,5 @@ let waves =
       return (fun () -> Hardcaml_waveterm_interactive.run (Cpu.Tests.waves ())))
 ;;
 
-let command =
-  Command.group ~summary:"Hardware dev tools" [ "compile", compile; "waves", waves ]
-;;
-
+let command = Command.group ~summary:"Hardware dev tools" [ "waves", waves ]
 let () = Command_unix.run command
