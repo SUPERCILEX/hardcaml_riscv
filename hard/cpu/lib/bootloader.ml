54a7af30af62be7999707793c548da04f1215a1e
Alex Saveau <saveau.alexandre@gmail.com>
2023-02-16 23:49:20 -0800

    Add first test program

--- hard/cpu/lib/bootloader.ml
+++ hard/cpu/lib/bootloader.ml
@@ -6,10 +6,17 @@ let bytes = List.init 8 ~f:(Signal.of_int ~width:8)
 
 module For_testing = struct
   module Sample_programs = struct
-    type t = Invalid [@@deriving sexp_of, compare, enumerate]
+    type t =
+      | Invalid
+      | Simple
+    [@@deriving sexp_of, compare, enumerate]
   end
 
   let sample = function
     | Sample_programs.Invalid -> List.init 8 ~f:(Signal.of_int ~width:8)
+    | Simple ->
+      [%blob "cpu/test_binaries/simple.bin"]
+      |> String.to_list
+      |> List.map ~f:Signal.of_char
   ;;
 end
---
+++ hard/cpu/test_binaries/simple.s
@@ -0,0 +1,5 @@
+addi zero, zero, 1
+addi a0, zero, 1
+li a1, 3
+sll a0, a0, a1
+add a0, a0, a1
