14e0ee9eace872c1174b1a800521f5c692c16a61
Alex Saveau <saveau.alexandre@gmail.com>
2023-02-18 16:32:32 -0800

    Generate more reports

--- hard/arty/scripts/reports.tcl
+++ hard/arty/scripts/reports.tcl
@@ -8,3 +8,6 @@ report_bus_skew -file bus_skew.rpt
 report_utilization -file utilization.rpt
 report_qor_suggestions -file suggestions.rpt
 report_design_analysis -complexity -congestion -timing -setup -hold -show_all -extend -qor_summary -file design_analysis.rpt
+report_power -file power.rpt 
+report_ram_utilization -file ram_utilization.rpt 
+report_high_fanout_nets -file high_fanout_nets.rpt
