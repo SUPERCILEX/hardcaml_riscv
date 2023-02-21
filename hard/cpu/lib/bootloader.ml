33ed2d00bb1f3f75953ec7ef58b884ab20430a45
Alex Saveau <saveau.alexandre@gmail.com>
2023-02-20 16:40:07 -0800

    Expand dune domain in preparation for boot loader infra
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- hard/cpu/lib/bootloader.ml
+++ hard/cpu/lib/bootloader.ml
@@ -1,6 +1,6 @@
 open! Core
 
-let bytes = [%blob "cpu/test_binaries/test.bin"]
+let bytes = [%blob "soft/boot.bin"]
 
 module For_testing = struct
   module Sample_programs = struct
@@ -16,10 +16,10 @@ module For_testing = struct
 
   let sample = function
     | Sample_programs.Invalid -> List.init 8 ~f:Char.of_int_exn |> String.of_char_list
-    | Simple -> [%blob "cpu/test_binaries/simple.bin"]
-    | String_search -> [%blob "cpu/test_binaries/strchr.bin"]
-    | Fibonacci -> [%blob "cpu/test_binaries/fibonacci.bin"]
-    | Atoi -> [%blob "cpu/test_binaries/atoi.bin"]
-    | Uart_echo -> [%blob "cpu/test_binaries/uart_echo.bin"]
+    | Simple -> [%blob "hard/cpu/test_binaries/simple.bin"]
+    | String_search -> [%blob "hard/cpu/test_binaries/strchr.bin"]
+    | Fibonacci -> [%blob "hard/cpu/test_binaries/fibonacci.bin"]
+    | Atoi -> [%blob "hard/cpu/test_binaries/atoi.bin"]
+    | Uart_echo -> [%blob "hard/cpu/test_binaries/uart_echo.bin"]
   ;;
 end
