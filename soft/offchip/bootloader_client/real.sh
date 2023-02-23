d8cf6e7a3a2ebe5679a16dd96627038e768b5052
Alex Saveau <saveau.alexandre@gmail.com>
2023-02-22 21:19:56 -0800

    Promote program bins and auto-configure the tty
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- soft/offchip/bootloader_client/real.sh
+++ soft/offchip/bootloader_client/real.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+[ $(stat -c "%a" /dev/ttyUSB1) -gt 665 ] || sudo chmod 666 /dev/ttyUSB1
+(cd ../tstp && cargo r -- -b 128000 /dev/ttyUSB1)
+
 [ ! -e "/tmp/command.out" ] && mkfifo "/tmp/command.out"
 
 ./hold_fd.py "/tmp/command.out" r &
