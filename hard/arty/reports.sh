1674e4829cc8a0b9581752577f6b4833a4292360
Alex Saveau <saveau.alexandre@gmail.com>
2023-03-09 12:54:45 -0800

    Move vivado sourcing to bashrc

--- hard/arty/reports.sh
+++ hard/arty/reports.sh
@@ -3,5 +3,4 @@ set -e
 
 export BOARD="arty-a7-100"
 
-source ~/xilinx/Vivado/2022.2/settings64.sh
 dune build @reports --auto-promote
--- hard/arty/upload.sh
+++ hard/arty/upload.sh
@@ -3,6 +3,5 @@ set -e
 
 export BOARD="arty-a7-100"
 
-source ~/xilinx/Vivado/2022.2/settings64.sh
 dune build top.bit --auto-promote
 djtgcfg prog --file top.bit -d Arty -i 0
